09b4df8c40eda1226292193de52d599acadada60
Mark McDowall <markm@iqmetrix.com>
2013-09-27 10:14:56 -0700

    initial commit

---
+++ Log4Mongo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace Log4Mongo
+{
+    class Program
+    {
+        private static readonly ILog _logger = LogManager.GetLogger(typeof (Program));
+
+        static void Main(string[] args)
+        {
+            _logger.Debug("Will not log.");
+            _logger.Info("Informational only");
+            _logger.Error("Error, does not compute.");
+            Console.ReadKey();
+        }
+    }
+}
